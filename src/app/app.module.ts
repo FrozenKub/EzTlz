fc0044755c52fd95c6be025f58db550d5d820e98
frozenkub <simachev.atnon@mail.ru>
2021-10-19 17:01:27 +0300

    [+] Entrance page ready

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
 
 import { AppComponent } from './app.component';
 import {MainComponent} from "./main/main.component";
+import {NzBreadCrumbModule} from "ng-zorro-antd/breadcrumb";
+import {NzMenuModule} from "ng-zorro-antd/menu";
+import { EntranceComponent } from './entrance/entrance.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainComponent
+    MainComponent,
+    EntranceComponent
   ],
   imports: [
-    BrowserModule
+    NzButtonModule,
+    NzLayoutModule,
+    BrowserModule,
+    NzBreadCrumbModule,
+    NzMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
